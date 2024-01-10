5b0c1014ebcda60c79e2a5acd189e6bab86c84da
Yasemin Bridges <y.bridges@qmul.ac.uk>
2024-01-10 16:34:27 +0000

    Add bash script for executing benchmark analysis

---
+++ benchmark/run_benchmark.sh
@@ -0,0 +1,10 @@
+TESTDATA_DIR=
+RESULTS_DIR=
+
+touch runs.txt
+
+for d in "$RESULTS_DIR"/* ; do
+    echo -e "${TESTDATA_DIR%/}/phenopackets\t$d" >> runs.txt
+done
+
+pheval-utils benchmark-comparison --run-data runs.txt --output-prefix Exomiser_vs_LLM_models --disease-analysis --gene-analysis
