c3f8538c5d87fb863fd75456a8889ba8d51af934
Yasemin Bridges <y.bridges@qmul.ac.uk>
2024-01-10 16:34:40 +0000

    remove hardcoded paths

--- tool_configurations/exomiser/run_exomiser.sh
+++ tool_configurations/exomiser/run_exomiser.sh
@@ -1,7 +1,7 @@
-INPUT_DIR=/Users/yaseminbridges/full_test_pheval_run/configurations/exomiser-13.3.0/2309/
-TESTDATA_DIR=/Users/yaseminbridges/Documents/GitHub/exomiser-llm-bench/corpora/LIRICAL
-OUTPUT_DIR=/Users/yaseminbridges/Documents/GitHub/exomiser-llm-bench/tool_configurations/exomiser
+INPUT_DIR=
+TESTDATA_DIR=
+OUTPUT_DIR=
 
-mkdir -p $OUTPUT_DIR/exomiser-13.3.0-2109
+mkdir -p "${OUTPUT_DIR%/}"/exomiser-13.3.0-2109
 
-pheval run --input-dir $INPUT_DIR --runner exomiserphevalrunner --testdata-dir $TESTDATA_DIR --output-dir $OUTPUT_DIR/exomiser-13.3.0-2109 --version 13.3.0
+pheval run --input-dir "$INPUT_DIR" --runner exomiserphevalrunner --testdata-dir "$TESTDATA_DIR" --output-dir "${OUTPUT_DIR%/}"/exomiser-13.3.0-2109 --version 13.3.0
