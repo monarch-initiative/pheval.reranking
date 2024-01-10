c04206bab4b3e03059b52608d6f66b1ba24fc6a0
Yasemin Bridges <y.bridges@qmul.ac.uk>
2024-01-10 14:34:56 +0000

    remove `results` sub directory

--- tool_configurations/exomiser/run_exomiser.sh
+++ tool_configurations/exomiser/run_exomiser.sh
@@ -2,6 +2,6 @@ INPUT_DIR=/Users/yaseminbridges/full_test_pheval_run/configurations/exomiser-13.
 TESTDATA_DIR=/Users/yaseminbridges/Documents/GitHub/exomiser-llm-bench/corpora/LIRICAL
 OUTPUT_DIR=/Users/yaseminbridges/Documents/GitHub/exomiser-llm-bench/tool_configurations/exomiser
 
-mkdir -p $OUTPUT_DIR/results/exomiser-13.3.0-2109
+mkdir -p $OUTPUT_DIR/exomiser-13.3.0-2109
 
-pheval run --input-dir $INPUT_DIR --runner exomiserphevalrunner --testdata-dir $TESTDATA_DIR --output-dir $OUTPUT_DIR/results/exomiser-13.3.0-2109 --version 13.3.0
+pheval run --input-dir $INPUT_DIR --runner exomiserphevalrunner --testdata-dir $TESTDATA_DIR --output-dir $OUTPUT_DIR/exomiser-13.3.0-2109 --version 13.3.0
