445bff9477b2e8a4aee2c93d6b78d4a1ff25f52c
Justin Reese <justaddcoffee@gmail.com>
2024-08-26 07:16:21 -0400

    Config config.yaml and run_exomiser.sh

--- tool_configurations/exomiser/run_exomiser.sh
+++ tool_configurations/exomiser/run_exomiser.sh
@@ -1,6 +1,6 @@
-INPUT_DIR=
-TESTDATA_DIR=
-OUTPUT_DIR=
+INPUT_DIR=~/PythonProject/exomiser-llm-bench/tool_configurations/exomiser
+TESTDATA_DIR=~/PythonProject/malco/supplemental_data/
+OUTPUT_DIR=/Users/jtr4v/PythonProject/exomiser-llm-bench/tool_configurations/exomiser/EXOMISER_PHENOTYPE_ONLY_2024_08_24
 
 mkdir -p "${OUTPUT_DIR%/}"/exomiser-13.3.0-2109
 
